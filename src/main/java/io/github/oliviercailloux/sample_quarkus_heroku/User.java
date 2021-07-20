5fcdcd978e1a8ac3cebb9d68f01ba4be1a59f874
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2021-07-20 19:10:19 +0200

    Unique users

--- src/main/java/io/github/oliviercailloux/sample_quarkus_heroku/User.java
+++ src/main/java/io/github/oliviercailloux/sample_quarkus_heroku/User.java
@@ -10,6 +10,7 @@ import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class User {
 
 	@Username
 	@NotNull
+	@Column(unique = true)
 	private String username;
 	@Password
 	@NotNull
