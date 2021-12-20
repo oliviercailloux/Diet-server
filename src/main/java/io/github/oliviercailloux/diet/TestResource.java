1e2784cdb5da3f949dab8c8b3c02c1fcae4307d3
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-12-20 17:02:37 +0100

    Test perfs resource

--- src/main/java/io/github/oliviercailloux/diet/TestResource.java
+++ src/main/java/io/github/oliviercailloux/diet/TestResource.java
@@ -1,5 +1,7 @@
 package io.github.oliviercailloux.diet;
 
+import com.google.common.base.Stopwatch;
+import io.github.oliviercailloux.diet.dao.Base64;
 import io.github.oliviercailloux.diet.entity.User;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
@@ -7,6 +9,7 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -22,6 +25,8 @@ public class TestResource {
 	SecurityContext securityContext;
 	@Inject
 	UserService userService;
+	@Inject
+	VideoService videoService;
 
 	@GET
 	@PermitAll
@@ -30,6 +35,89 @@ public class TestResource {
 	public String getUnauthenticated() {
 		LOGGER.info("Getting unauthenticated.");
 		return "Unauthenticated.";
+	}
+
+	@GET
+	@PermitAll
+	@Path("/perf")
+	@Produces({ MediaType.TEXT_PLAIN })
+	@Transactional
+	public String getPerf() {
+		LOGGER.info("Getting perf.");
+
+		final Stopwatch stopwatch = Stopwatch.createStarted();
+		userService.getWithoutEvents(Base64.from("user0"));
+		stopwatch.stop();
+		LOGGER.warn("Time for one simple query: {} ms.", stopwatch.elapsed().toMillis());
+
+		return "Perf.";
+	}
+
+	@GET
+	@PermitAll
+	@Path("/perfsSimple/{nbIterations}")
+	@Produces({ MediaType.TEXT_PLAIN })
+	@Transactional
+	public String getPerfsSimple(@PathParam("nbIterations") int nbIterations) {
+		LOGGER.info("Getting perfs.");
+		final String queries = nbIterations == 1 ? "query" : "queries";
+		{
+			final Stopwatch stopwatch = Stopwatch.createStarted();
+			for (int i = 0; i < nbIterations; ++i) {
+				userService.getWithoutEvents(Base64.from("user0"));
+			}
+			stopwatch.stop();
+			LOGGER.warn("Average time over {} simple " + queries + ": {} ms.", nbIterations,
+					stopwatch.elapsed().dividedBy(nbIterations).toMillis());
+		}
+		return "Perfs.";
+	}
+
+	@GET
+	@PermitAll
+	@Path("/perfs/{nbIterations}")
+	@Produces({ MediaType.TEXT_PLAIN })
+	@Transactional
+	public String getPerfs(@PathParam("nbIterations") int nbIterations) {
+		LOGGER.info("Getting perfs.");
+		final String queries = nbIterations == 1 ? "query" : "queries";
+		{
+			final Stopwatch stopwatch = Stopwatch.createStarted();
+			for (int i = 0; i < nbIterations; ++i) {
+				userService.getWithoutEvents(Base64.from("user0"));
+			}
+			stopwatch.stop();
+			LOGGER.warn("Average time over {} simple " + queries + ": {} ms.", nbIterations,
+					stopwatch.elapsed().dividedBy(nbIterations).toMillis());
+		}
+		{
+			final Stopwatch stopwatch = Stopwatch.createStarted();
+			for (int i = 0; i < nbIterations; ++i) {
+				userService.get(Base64.from("user0"));
+			}
+			stopwatch.stop();
+			LOGGER.warn("Average time over {} complex " + queries + ": {} ms.", nbIterations,
+					stopwatch.elapsed().dividedBy(nbIterations).toMillis());
+		}
+		{
+			final Stopwatch stopwatch = Stopwatch.createStarted();
+			for (int i = 0; i < nbIterations; ++i) {
+				userService.getWithoutEvents(Base64.from("user0"));
+			}
+			stopwatch.stop();
+			LOGGER.warn("Average time over {} simple " + queries + ": {} ms.", nbIterations,
+					stopwatch.elapsed().dividedBy(nbIterations).toMillis());
+		}
+		{
+			final Stopwatch stopwatch = Stopwatch.createStarted();
+			for (int i = 0; i < nbIterations; ++i) {
+				videoService.getAll();
+			}
+			stopwatch.stop();
+			LOGGER.warn("Average time over {} complex video " + queries + ": {} ms.", nbIterations,
+					stopwatch.elapsed().dividedBy(nbIterations).toMillis());
+		}
+		return "Perfs.";
 	}
 
 	@GET
