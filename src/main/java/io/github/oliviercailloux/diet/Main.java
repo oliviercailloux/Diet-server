fb797079f0a91855d67613f4af244600ef262b8d
Olivier Cailloux <olivier.cailloux@gmail.com>
2022-12-16 21:51:43 +0100

    Main that catches SIGTERM

--- src/main/java/io/github/oliviercailloux/diet/Main.java
+++ src/main/java/io/github/oliviercailloux/diet/Main.java
@@ -3,22 +3,25 @@ package io.github.oliviercailloux.diet;
 import static com.google.common.base.Verify.verify;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.Signal;
 
+@QuarkusMain
 public class Main {
 	@SuppressWarnings("unused")
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("Installing signal handler.");
 		Signal.handle(new Signal("TERM"), Main::handleSignal);
 		Quarkus.run(args);
 	}
 
 	public static void handleSignal(Signal signal) {
-		LOGGER.warn("Got signal {}.", signal);
-		verify(signal.getName().equals("TERM"));
+		LOGGER.info("Got TERM signal {}.", signal, "exitting with zero");
+		verify(signal.getNumber() == 15);
 		System.exit(0);
 	}
 }
